fd8d114d3f28e4439e8c7924451e52319db31efc
Jorge Silva <jorge@fcn.edu.br>
2018-09-12 09:05:46 -0300

    scraping

--- src/run.py
+++ src/run.py
@@ -1,1 +1,9 @@
-print( "teste" )
+# -*- coding: UTF-8 -*-
+#
+import pandas as pd
+
+
+path = "../data/terremotos_ultimos10_anos.csv"
+df = pd.read_csv(path, skiprows = 1, thousands=',')
+
+print(df.head(5))
