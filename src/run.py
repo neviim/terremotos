744747bef8c556304bf4eb8c6b0aa4032f90e9c6
Jorge Silva <jorge@fcn.edu.br>
2018-09-13 10:14:55 -0300

    fix

--- src/run.py
+++ src/run.py
@@ -1,9 +1,12 @@
 # -*- coding: UTF-8 -*-
 #
+# conda activate py37
+# cd C:\Users\jorge.FCN\Documents\GitHub\_python_projetos
+#
 import pandas as pd
 
 
 path = "../data/terremotos_ultimos10_anos.csv"
-df = pd.read_csv(path, skiprows = 1, thousands=',')
+df = pd.read_csv(path, skip, thousands=',')
 
 print(df.head(5))
