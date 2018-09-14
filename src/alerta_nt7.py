2348d958c472de5f8c5bc900619058b52b70367d
Jorge Silva <jorge@fcn.edu.br>
2018-09-14 14:31:47 -0300

    Ajustes dependencias
    Pacotes de dependencias para o projeto.

--- src/alerta_nt7.py
+++ src/alerta_nt7.py
@@ -1,10 +1,12 @@
 # -*- coding: UTF-8 -*-
 #
-# $ cd /home/jorge/src/terremotos/src
-# $ source ~/virtual/py36/bin/activate
-# (py36) ➜
+#   Ambiente de desemvolvimento.
 #
-__autor__ = "neviim jads - 2018"
+#       $ cd /home/jorge/src/terremotos/src
+#       $ source ~/virtual/py36/bin/activate
+#       (py36) ➜
+#
+__autor__ = "neviim jads - 09/2018"
 
 from hashlib import md5, sha1
 from lxml import html
@@ -12,23 +14,19 @@ import requests
 import json
 
 class AlertaNt7Terremotos(object):
-    def __init__(self):
-        super(AlertaNt7Terremotos. self).__init__()
-        self.arg = arg
-        self.url = 'http://monitorglobal.com.br/terremotos.html'
+    def __init__(self, url):
+        super(AlertaNt7Terremotos, self).__init__()
+        self.url = url
 
-    def scraping_terremotos(self, purl):
+    def scraping(self):
         """ Estrai a lista com os ultimos 200 terremotos ocorridos.
         
-            Arguments:
-                purl {[str]} -- [string com a url do site a ser aplicado o scraping]
-
             Retorno:
                 parametro1 [{borlean}] -- [campo logico True caso tudo corra bem e falso se der algo errado]
                 parametro2 [{lista}]   -- [lista com todos os 200 dados da lista de ultimos terremotos]
         """
         dados  = []
-        pagina = requests.get(purl)
+        pagina = requests.get(self.url)
 
         # se pagina for encontrada.
         if pagina.status_code == 200:
@@ -38,7 +36,7 @@ class AlertaNt7Terremotos(object):
             div_main = tree.xpath('//div[@id="sismo_lista"]')
 
             # não ouvendo dados, retorna false.
-            if div_main is Nome:
+            if div_main is None:
                 return(False)
 
             # cria uma lista com os dados.
@@ -54,6 +52,16 @@ class AlertaNt7Terremotos(object):
         return sha1(texto.encode(encoding)).hexdigest()
 
         
+# -- main, 
+def main():
+    url = 'http://monitorglobal.com.br/terremotos.html'
 
+    terremotos = AlertaNt7Terremotos(url)
+    dados = terremotos.scraping()
 
+    print(dados)
+
+# -- inicio
+if __name__ == '__main__':
+    main()
 
