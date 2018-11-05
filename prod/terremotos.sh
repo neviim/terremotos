b988f9569fb167c678d2b15facb499903dee3d4e
Jorge Silva <jorge@fcn.edu.br>
2018-11-05 15:02:39 -0200

    Ajuste

--- prod/terremotos.sh
+++ prod/terremotos.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 # virtualenv agora esta ativ0
-# /home/jorge/src/terremotos/src/src
+# /home/neviim/src/_projetos/terremotos/src
 #
 # A cada 30 minutos sera executado, coloque a linha abaixo no crontab.
 #
